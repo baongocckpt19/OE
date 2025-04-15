62ce1ed1cbb7b11ca393d01c4e971565bd726114
Khoin06 <ngokhoinsl@gmail.com>
2025-04-15 07:12:36 -0700

    add nút xóa questions

--- backend/src/main/java/com/example/backend/controller/QuestionController.java
+++ backend/src/main/java/com/example/backend/controller/QuestionController.java
@@ -1,43 +1,49 @@
 package com.example.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.model.Question;
+import com.example.backend.model.Student;
 import com.example.backend.repository.QuestionRepository;
+import com.example.backend.services.QuestionService;
 
 @RestController
 @RequestMapping("/api/questions")
 @CrossOrigin(origins = "*")
 public class QuestionController {
-    private final QuestionRepository repo;
+    private final QuestionService questionService;
 
-    public QuestionController(QuestionRepository repo) {
-        this.repo = repo;
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
     }
 
     @GetMapping("")
-    public List<Question> getAllQuestions() {
-        return repo.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        List<Question> question = questionService.getAllQuestions();
+        return new ResponseEntity<>(question, HttpStatus.OK);
     }
 
-    @GetMapping("/filter")
-    public List<Question> getFilteredQuestions(
-            @RequestParam(required = false) String subject,
-            @RequestParam(required = false) String difficulty) {
 
-        if (subject != null && difficulty != null) {
-            return repo.findBySubjectAndDifficulty(subject, difficulty);
-        } else if (subject != null) {
-            return repo.findBySubject(subject);
-        } else if (difficulty != null) {
-            return repo.findByDifficulty(difficulty);
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteQuestion(@PathVariable Long id) {
+       try{
+        questionService.deleteQuestion(id); // Sau đó xóa câu hỏi
+            return new ResponseEntity<>("Câu hỏi và các chi tiết liên quan đã được xóa.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return repo.findAll();
     }
+    
 }
--- backend/src/main/java/com/example/backend/repository/QuestionRepository.java
+++ backend/src/main/java/com/example/backend/repository/QuestionRepository.java
@@ -3,13 +3,16 @@ package com.example.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.backend.model.Question;
 
+import jakarta.transaction.Transactional;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-        @Query(value = "SELECT * FROM question_bank  WHERE name_of_subject = :subject", nativeQuery = true)
+    @Query(value = "SELECT * FROM question_bank  WHERE name_of_subject = :subject", nativeQuery = true)
     List<Question> findBySubject(@Param("subject") String subject);
 
     @Query(value = "SELECT * FROM question_bank WHERE difficulty = :difficulty", nativeQuery = true)
@@ -17,7 +20,22 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     @Query(value = "SELECT * FROM question_bank WHERE name_of_subject = :subject AND difficulty = :difficulty", nativeQuery = true)
     List<Question> findBySubjectAndDifficulty(
-        @Param("subject") String subject,
-        @Param("difficulty") String difficulty
-    );
+            @Param("subject") String subject,
+            @Param("difficulty") String difficulty);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM dbo.user_answers WHERE question_id = :id", nativeQuery = true)
+    void deleteUserAnswersByQuestionId(@Param("id") Long id); // Thêm phương thức này
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM dbo.questions  WHERE question_bank_id = :id", nativeQuery = true)
+    void deleteQuestionDetailsByQuestionId(@Param("id") Long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM dbo.question_bank WHERE question_id = :id", nativeQuery = true)
+    void deleteByQuestionId(@Param("id") Long id);
+
 }
